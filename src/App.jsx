5eee86ef43f1ccb54fce349990ab1c5dcc7d9c71
Merve Sultan Darıcı <sultanasya51@gmail.com>
2024-12-09 21:48:57 +0300

    onAuthStateChanged

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes";
 import { Toaster } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
+import { setUser } from "./redux/slices/authSlice";
+import { useEffect } from "react";
 
 
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if(currentUser){
+        dispatch(setUser(currentUser));
+      }else{
+        dispatch(setUser(null));
+      }
+        
+     })
+     return () => unsubscribe()
+ }, [dispatch])
+
+
+
   return (
     <div>
     <RouterProvider router={router} />
